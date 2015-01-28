9d40ec7d04dbd7d3a5a1fb876a7c702106a439d8
David Culler <culler@berkeley.edu>
2015-01-28 13:32:55 -0800

    udpate base to include button

--- app/base.lua
+++ app/base.lua
@@ -3,13 +3,22 @@ require("cord") -- scheduler / fiber library
 print ("Base  test ")
 
 sh = require "stormsh"
-LED = require("LED")
+LED = require("led")
+Button = require("button")
 
--- blue LED plugged into LED board attached to shield pin D2
-
+-- blue LED plugged into LED board attached to base shield pin D2
+-- button plugged into base shield pin D3
 blue = LED:new("D2")
+btn = Button:new("D3")
 
 blue:flash(5)
+
+count = 1
+btn:whenever("RISING", function() 
+		print("BTN", btn:pressed(), count) 
+		blue:flash(count % 4)
+		count = count+1
+		       end)
 
 -- start a shell so you can play more
 
