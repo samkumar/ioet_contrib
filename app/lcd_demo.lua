bf0f21b62bd515aaeef894b1b0cb5cc87e0d263e
Sam Kumar <samkumar@berkeley.edu>
2015-02-04 17:41:48 -0800

    Made I2C scanner more general and tweaked demo a bit

--- app/lcd_demo.lua
+++ app/lcd_demo.lua
@@ -1,16 +1,19 @@
 LCD = require "lcd"
 
+lcd = LCD:new(storm.i2c.EXT, 0x7c, storm.i2c.EXT, 0xc4)
 cord.new(function ()
-    LCD.init(2, 1)
-    LCD.writeString("THE INTERNET OF")
-    LCD.setCursor(1, 0)
-    LCD.writeString("THINGS, Spr '15")
+    print("started")
+    lcd:init(2, 1)
+    print("initialized")
+    lcd:writeString("THE INTERNET OF")
+    lcd:setCursor(1, 0)
+    lcd:writeString("THINGS, Spr '15")
     while true do
-        LCD.setBackColor(255, 255, 255)
+        lcd:setBackColor(255, 255, 255)
         cord.await(storm.os.invokeLater, storm.os.SECOND)
-        LCD.setBackColor(0, 255, 0)
+        lcd:setBackColor(0, 255, 0)
         cord.await(storm.os.invokeLater, storm.os.SECOND)
-        LCD.setBackColor(0, 0, 255)
+        lcd:setBackColor(0, 0, 255)
         cord.await(storm.os.invokeLater, storm.os.SECOND)
     end
 end)
