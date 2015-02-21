8efb6ce75e613a52d2524ce1e4544c9918549446
Sam Kumar <samkumar@berkeley.edu>
2015-02-21 14:09:53 -0800

    Finished implementing basic discovery service; added demo

--- lib/discoverer.lua
+++ lib/discoverer.lua
@@ -7,6 +7,8 @@ function Discoverer:new(service_found, service_lost, timeout, dport, iport)
     timeout = timeout or 300 * storm.os.SECOND
     dport = dport or 1525
     iport = iport or 1526
+    self.dport = dport
+    self.iport = iport
     setmetatable(self, {})
     
     -- Maps an ip address to a list of services
@@ -15,21 +17,17 @@ function Discoverer:new(service_found, service_lost, timeout, dport, iport)
     
     self.dsock = storm.net.udpsocket(dport, function (payload, ip, port)
         payload = storm.mp.unpack(payload)
-        if not self.discovered_services[ip] then
-            self.discovered_services[ip] = {}
-            self.timeouts[ip] = {}
-        end
-        local old_payload = self.discovered_services[ip][port]
-        local old_timeout = self.timeouts[ip][port]
+        local old_payload = self.discovered_services[ip]
+        local old_timeout = self.timeouts[ip]
         if old_timeout then
             storm.os.cancel(old_timeout)
         end
         local discovered_services = self.discovered_services
         local timeouts = self.timeouts
-        self.timeouts[ip][port] = storm.os.invokeLater(timeout, function ()
-            local toremove = discovered_services[ip][port]
-            discovered_services[ip][port] = nil
-            timeouts[ip][port] = nil
+        self.timeouts[ip] = storm.os.invokeLater(timeout, function ()
+            local toremove = discovered_services[ip]
+            discovered_services[ip] = nil
+            timeouts[ip] = nil
             for k, v in pairs(toremove) do
                 if k ~= "id" then
                     service_lost(ip, port, toremove["id"], k, v)
@@ -54,7 +52,7 @@ function Discoverer:new(service_found, service_lost, timeout, dport, iport)
                 end
             end
         end
-        self.discovered_services[ip][port] = payload
+        self.discovered_services[ip] = payload
     end)
     
     self.nqc = NQC:new(iport)
@@ -62,16 +60,16 @@ function Discoverer:new(service_found, service_lost, timeout, dport, iport)
     return self
 end
 
-function Discoverer:invoke(ip, port, name, args, callback)
-    local msg = storm.mp.pack({name, args})
-    self.nqc:sendMessage(msg, ip, port, 1500, 50 * storm.os.MILLISECOND, nil, function (message, ip, port)
+function Discoverer:invoke(ip, name, args, callback)
+    local msg = {name, args}
+    self.nqc:sendMessage(msg, ip, self.iport, 1500, 50 * storm.os.MILLISECOND, nil, function (message, ip, port)
         message["_id"] = nil
         callback(message, ip, port)
     end)
 end
 
 function Discoverer:close()
-    self.dsock.close()
+    storm.net.close(self.dsock)
     self.nqc:close()
 end
 
