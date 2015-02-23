9322eee8fb72bb8ba89702d64615d75015d5551f
Jack He <siyuanhe@berkeley.edu>
2015-02-22 19:36:23 -0800

    Improved the i2creg library by allowing multiple read and write, this update is backward compatible with existing i2creg library.

--- lib/i2creg.lua
+++ lib/i2creg.lua
@@ -1,54 +1,54 @@
 require "cord"
+require "table"
+
 local REG = {}
-
---[[
-
-The REG register type represents the registers associated with at port PORT
-(storm.i2c.INT or storm.i2c.EXT) and address ADDRESS.
-
-It provides an abstraction for reading and writing to registers.
-
-]]--
 
 -- Create a new I2C register binding
 function REG:new(port, address)
-    obj = {port=port, address=address}
+    local obj = {port=port, address=address}
     setmetatable(obj, self)
     self.__index = self
     return obj
 end
 
--- Read a given register address
-function REG:r(reg)
-    -- create array with address
+-- Read from a given register address
+-- REG: the register address to be read from
+-- NUM: the number of bytes to be read [OPTIONAL]
+-- Return: a storm array of UNIT8
+function REG:r(reg, num)
+    num = num or 1
     local arr = storm.array.create(1, storm.array.UINT8)
     arr:set(1, reg)
-    -- write address
-    local rv1 = cord.await(storm.i2c.write, self.port + self.address, storm.i2c.START, arr)
-    if rv1 ~= storm.i2c.OK then
-        print("Could not communicate with register: " .. rv1)
-        return nil
+    local rv = cord.await(storm.i2c.write, self.port + self.address, storm.i2c.START, arr)
+    if (rv ~= storm.i2c.OK) then
+        print ("ERROR ON I2C: ",rv)
     end
-    -- read register with RSTART
-    local rv2 = cord.await(storm.i2c.read, self.port + self.address, storm.i2c.RSTART + storm.i2c.STOP, arr)
-    -- check all return values
-    if rv2 ~= storm.i2c.OK then
-        print("Could not read from register: " .. rv2)
-        return nil
+    local result = storm.array.create(num, storm.array.UINT8)
+    rv = cord.await(storm.i2c.read, self.port + self.address, storm.i2c.RSTART + storm.i2c.STOP, result)
+    if (rv ~= storm.i2c.OK) then
+        print ("ERROR ON I2C: ",rv)
     end
-    return arr:get(1)
+    return result
 end
 
-function REG:w(reg, value)
-    -- create array with address and value
-    local arr = storm.array.create(2, storm.array.UINT8)
-    arr:set(1, reg)
-    arr:set(2, value)
-    -- write
+-- Write to a given register address
+-- REG: the register address to be written to
+-- VALUES: a lua 1-dimentional list of UINT8 values to be written to OR just 1 UINT8 lua value
+-- No return value
+function REG:w(reg, values)
+    if (type(values) ~= "table") then
+        values = {values}
+    end
+    local arr = storm.array.create(table.maxn(values) + 1, storm.array.UINT8)
+    arr:set(1,reg)
+    local i = 2
+    for key, value in pairs(values) do
+        arr:set(i, value)
+        i = i + 1
+    end
     local rv = cord.await(storm.i2c.write, self.port + self.address, storm.i2c.START + storm.i2c.STOP, arr)
-    -- check return value
-    if rv ~= storm.i2c.OK then
-        print("Could not write to register: " .. rv)
+    if (rv ~= storm.i2c.OK) then
+        print ("ERROR ON I2C: ",rv)
     end
 end
 
